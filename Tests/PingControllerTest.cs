4865cccb8d2ffb53ce57df6e574bb468cdcfde5e
Eric Mignot <eric.mignot@gmail.com>
2013-11-02 21:04:30 -0400

    PingControllerTest added

--- Tests/PingControllerTest.cs
+++ Tests/PingControllerTest.cs
@@ -1,14 +1,25 @@
-using System;
 using NUnit.Framework;
+using System.Web.Mvc;
+using Yose.Controllers;
 
-namespace Tests
+namespace Yose
 {
-    [TestFixture()]
-    public class Test
+    [TestFixture]
+    public class PingControllerTest
     {
-        [Test()]
-        public void TestCase()
+        [Test]
+        public void ReturnsJsonResult()
         {
+            Assert.That(new PingController().Index(), Is.InstanceOf<JsonResult>());
+        }
+
+        [Test]
+        public void ReturnsValueExpetedByYose()
+        {
+            var response = (JsonResult)new PingController().Index();
+            var expected = new JsonResult { Data = new { alive = true } };
+
+            Assert.That(response.Data.ToString(), Is.EqualTo(expected.Data.ToString()));
         }
     }
 }
